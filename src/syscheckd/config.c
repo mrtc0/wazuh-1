Gary5556/wazuh

Add the parent process name int Whodata alerts in Linux

src/syscheckd/config.c
@@ -131,6 +131,7 @@ void free_whodata_event(whodata_evt *w_evt) {
     if (w_evt->group_id) free(w_evt->group_id);
     if (w_evt->path) free(w_evt->path);
     if (w_evt->process_name) free(w_evt->process_name);
+    if (w_evt->pprocess_name) free(w_evt->pprocess_name);
     if (w_evt->inode) free(w_evt->inode);
     if (w_evt->dev) free(w_evt->dev);
     free(w_evt);

src/syscheckd/create_db.c
@@ -967,6 +967,7 @@ cJSON * fim_audit_json(const whodata_evt * w_evt) {
     cJSON_AddStringToObject(fim_audit, "audit_name", w_evt->audit_name);
     cJSON_AddStringToObject(fim_audit, "effective_uid", w_evt->effective_uid);
     cJSON_AddStringToObject(fim_audit, "effective_name", w_evt->effective_name);
+    cJSON_AddStringToObject(fim_audit, "pprocess_name", w_evt->pprocess_name);
     cJSON_AddNumberToObject(fim_audit, "ppid", w_evt->ppid);
 #endif
 

mrtc0/wazuh-1

src/syscheckd/config.c
@@ -131,6 +131,8 @@ void free_whodata_event(whodata_evt *w_evt) {
     if (w_evt->group_id) free(w_evt->group_id);
     if (w_evt->path) free(w_evt->path);
     if (w_evt->process_name) free(w_evt->process_name);
+    if (w_evt->parent_name) free(w_evt->parent_name);
+    if (w_evt->parent_cwd) free(w_evt->parent_cwd);
     if (w_evt->inode) free(w_evt->inode);
     if (w_evt->dev) free(w_evt->dev);
     free(w_evt);

src/syscheckd/create_db.c
@@ -967,6 +967,8 @@ cJSON * fim_audit_json(const whodata_evt * w_evt) {
     cJSON_AddStringToObject(fim_audit, "audit_name", w_evt->audit_name);
     cJSON_AddStringToObject(fim_audit, "effective_uid", w_evt->effective_uid);
     cJSON_AddStringToObject(fim_audit, "effective_name", w_evt->effective_name);
+    cJSON_AddStringToObject(fim_audit, "parent_name", w_evt->parent_name);
+    cJSON_AddStringToObject(fim_audit, "parent_cwd", w_evt->parent_cwd);
     cJSON_AddNumberToObject(fim_audit, "ppid", w_evt->ppid);
 #endif
 